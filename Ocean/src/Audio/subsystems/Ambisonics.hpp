c716d0564d6bc28dac0e54bfd360afdd2c4542c9
uniqueident <mihmm@spu.edu>
2024-12-02 11:39:16 -0800

    gonna restart this is unrecoverable.

--- Ocean/src/Audio/subsystems/Ambisonics.hpp
+++ Ocean/src/Audio/subsystems/Ambisonics.hpp
@@ -7,14 +7,32 @@ namespace Ocean{
         class Ambisonics{
 
             public:
-
-            Ambisonics(IPLContext& context,IPLAudioSettings& audiosettings, u8 maxOrder);
+            //for audio emitted by a point source.
+            Ambisonics(IPLContext& context,IPLAudioSettings& audiosettings,IPLHRTF& hrtf,u8 maxOrder);
+            void Effect_Encode(IPLAudioBuffer& inbuffer, IPLAudioBuffer& outbuffer,IPLVector3 space_xyz);
+            void Effect_Decode(IPLAudioBuffer& inbuffer, IPLAudioBuffer& outbuffer,IPLCoordinateSpace3 listener_xyz);
+            void orientation(IPLVector3 right_vec,IPLVector3 up_vec,IPLVector3 ahead_vec);
+            void orientation(IPLVector3 origin_vec,IPLVector3 right_vec,IPLVector3 up_vec,IPLVector3 ahead_vec);
             ~Ambisonics();
             private:
+            u8 maxOrder = 0;
+            //references if needed.
             IPLContext ref_context =nullptr;
             IPLAudioSettings ref_audio;
-            IPLAmbisonicsEncodeEffectSettings effectSettings{};
-            IPLAmbisonicsEncodeEffect effect = nullptr;
+            IPLHRTF ref_hrtf = nullptr;
+
+            //holds the effect settings
+                //encode
+            IPLAmbisonicsEncodeEffectSettings encode_Settings{};
+            IPLAmbisonicsEncodeEffectParams encode_params{};
+            IPLAmbisonicsEncodeEffect encode_effect = nullptr;
+                //decode
+            IPLAmbisonicsDecodeEffectSettings decode_Settings{};
+            IPLAmbisonicsDecodeEffectParams decode_params{};
+            IPLAmbisonicsDecodeEffect decode_effect = nullptr;
+
+        
+            
         };
     }
 }
