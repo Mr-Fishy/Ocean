e3bfac1852ce7d70ca5d7f06a3f58988512530aa
uniqueident <mateomihm@hotmail.com>
2025-01-21 00:36:52 -0800

    small changes

--- Ocean/src/Ocean/Audio/subsections/convert.cpp
+++ Ocean/src/Ocean/Audio/subsections/convert.cpp
@@ -1,5 +1,13 @@
 #include "convert.hpp"
 
+sonar::convert::convert(int number_channels, int samples){
+    buffer.numChannels = number_channels;
+    buffer.numSamples = samples;
+}
+sonar::convert::~convert(){
+
+}
+
 
 void sonar::convert::interleave(float* inData){
 
@@ -10,6 +18,7 @@ void sonar::convert::deinterleave(float* outData){
 
     return;
 }
+//This should be called if it needs to be used again for something else, basically when its done with its process.
 void sonar::convert::wipe(){
 
     return;
--- Ocean/src/Ocean/Audio/subsections/convert.hpp
+++ Ocean/src/Ocean/Audio/subsections/convert.hpp
@@ -8,17 +8,20 @@
 namespace sonar{
     class convert{
         public: 
-
+            convert(int number_channels =2, int samples = 1024);
+            ~convert();
             /*
             This calls the steam interleave/deinterleave functions :)
              */
             
-            void interleave(float* inData);
-            void deinterleave(float* outData);
+            void interleave(float* outData);
+            void deinterleave(float* inData);
+
+
 
             void wipe();
         private:
-            
+
             IPLAudioBuffer buffer;
 
             float* inData;
