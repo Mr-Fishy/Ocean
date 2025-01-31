e8533f6db1d175d9918751067c0af0acb70ba10a
uniqueident <mihmm@spu.edu>
2025-01-31 15:17:26 -0800

    more changes

--- Ocean/src/Ocean/Audio/steamaudio_dataholding.hpp
+++ Ocean/src/Ocean/Audio/steamaudio_dataholding.hpp
@@ -1,16 +1,20 @@
+#pragma once
 #include "Ocean/Types/SmartPtrs.hpp"
 #include "audio.hpp"
-#include "subsections/Binaural.hpp"
-#include "subsections/HRTF.hpp"
 #include <phonon.h>
 #include <unordered_map>
 
 namespace sonar{
+    //classes with no definitions.
+    class HRTF;
+    class Binaural;
+
     struct global_audio_context{
         static sonar::steamaudio* audio;
         std::unordered_map<const char*, Ref<sonar::HRTF>> hrtfs;
         std::unordered_map<const char*, Ref<sonar::Binaural>> binaural;
         
-        
     };
+
+    
 }
---
+++ Ocean/src/Ocean/Audio/subsections/HRTF.cpp
@@ -0,0 +1,3 @@
+#pragma once
+#include "HRTF.hpp"
+
