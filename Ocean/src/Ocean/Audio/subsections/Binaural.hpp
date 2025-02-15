649b4e86c84e01b4c22b3b7e774ecb6847b3a792
uniqueident <mateomihm@hotmail.com>
2025-02-15 00:15:09 -0800

    fixed some Binaural

--- Ocean/src/Ocean/Audio/subsections/Binaural.hpp
+++ Ocean/src/Ocean/Audio/subsections/Binaural.hpp
@@ -6,14 +6,16 @@ namespace sonar{
     //per new spacialized sound source.
     class Binaural{
         public:
-        Binaural(Ref<sonar::HRTF> hrtf);
+        Binaural(Ref<sonar::HRTF> hrtf,Ref<IPLAudioBuffer> inbuffer);
         ~Binaural();
         //gets the pointer for the class
-        void start(IPLBinauralEffectParams* params);
+        void spatialize(IPLBinauralEffectParams* params, IPLAudioBuffer* outbuffer);
 
         IPLBinauralEffect get_ptr(){return effect;}
 
         private:
+            IPLBinauralEffectSettings effectsettings{};
+            IPLAudioBuffer* buffer = nullptr;
             IPLBinauralEffect effect  =nullptr;
             IPLVector3 direction{0,0,0};
 
